049168a93b93e4e168c449fd8f86c06a759101b6
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-15 01:53:00 +0900

    edit main.go

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
-package main
-
-// @snip <header/import.go>
-// @snip <header/typedef.go>
-// @snip <utils/read.go>
-// @snip <utils/stream.go>
+// @snip <./header/import.go>
+// @snip <./header/typedef.go>
+// @snip <./utils/read.go>
+// @snip <./utils/stream.go>
+// @snip <./utils/min.go>
 
 func main() {
   sc.Init(Stdin())
