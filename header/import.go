e0e17aeecd1e1517ecb641fc18bb4d349fd7cda1
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-16 21:30:36 +0900

    delete all source

--- header/import.go
+++
@@ -1,8 +0,0 @@
-// @begin_cut
-package header
-// @end_cut
-import "fmt"
-import "io"
-import "os"
-import "strings"
-import "text/scanner"
--- header/typedef.go
+++
@@ -1,6 +0,0 @@
-// @begin_cut
-package header
-// @end_cut
-type Int int64
-type IntSlice []Int
-
